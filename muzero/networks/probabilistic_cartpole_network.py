585ad7edc1d43c59ba4a1faefe3c1793aa6549da
Dirichi <din206@nyu.edu>
2022-06-08 00:47:39 +0100

    Fix regularizers

--- muzero/networks/probabilistic_cartpole_network.py
+++ muzero/networks/probabilistic_cartpole_network.py
@@ -53,10 +53,10 @@ class ProbabilisticCartPoleNetwork(BaseNetwork):
         policy_network = Sequential([Dense(hidden_neurons, activation='relu', kernel_regularizer=regularizer),
                                      Dense(action_size, kernel_regularizer=regularizer)])
         dynamic_network = Sequential([
-          tfp.layers.DenseVariational(hidden_neurons, posterior_mean_field, prior_trainable, kl_weight=0.01,
-                            activation='relu', kernel_regularizer=regularizer),
-          tfp.layers.DenseVariational(representation_size, posterior_mean_field, prior_trainable, kl_weight=0.01,
-                            activation=representation_activation, kernel_regularizer=regularizer),
+          tfp.layers.DenseVariational(hidden_neurons, posterior_mean_field, prior_trainable, kl_weight=0.001,
+                            activation='relu', activity_regularizer=regularizer),
+          tfp.layers.DenseVariational(representation_size, posterior_mean_field, prior_trainable, kl_weight=0.001,
+                            activation=representation_activation, activity_regularizer=regularizer),
           # tfp.layers.DistributionLambda(lambda t: tfd.Normal(loc=t, scale=1)),
         ])
         reward_network = Sequential([Dense(16, activation='relu', kernel_regularizer=regularizer),
