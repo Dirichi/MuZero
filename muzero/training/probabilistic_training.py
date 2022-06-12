Fix masking in training

muzero/training/probabilistic_training.py
@@ -57,7 +57,7 @@ def update_weights(optimizer: tf.keras.optimizers, network: BaseNetwork, batch):
             target_value_batch = tf.boolean_mask(target_value_batch, mask)
             target_reward_batch = tf.boolean_mask(target_reward_batch, mask)
             target_next_representation_batch = network.representation_network(np.array(target_next_state_batch))
-            target_next_representation_batch = tf.boolean_mask(target_next_representation_batch, dynamic_mask)
+            target_next_representation_batch = tf.boolean_mask(target_next_representation_batch, mask)
             # Creating conditioned_representation: concatenate representations with actions batch
             actions_batch = tf.one_hot(actions_batch, network.action_size)
 